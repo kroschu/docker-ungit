31f9308da193aa5e08d10086bc1fa8e13ab7af8c
romcheck <romcheck@gmail.com>
2016-06-06 11:47:23 +0200

    nice tool

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eo pipefail
+
+if [ "${1:0:1}" = '-' ]; then
+    set -- ungit --no-launchBrowser --no-logRESTRequests "$@"
+fi
+
+if [ $UID ]; then
+    if ! id $UID >/dev/null 2>&1; then
+        useradd --shell /bin/bash --create-home --home-dir /repos --uid $UID --gid www-data ungit
+    fi
+umask 0002
+exec gosu $UID "$@"
+
+fi
+
+exec "$@"
